fed28738ca0d1f006717427265c90b1920ba02fe
Forrest Zhou <lzhou@ucsb.edu>
2025-04-19 23:51:32 -0700

    forrest - add PUT method to Menu Item Controller

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
@@ -96,4 +96,30 @@ public class UCSBDiningCommonsMenuItemController extends ApiController{
 
         return ucsbDiningCommonsMenuItem;
     }
+
+    /**
+     * Update a single menu item
+     * 
+     * @param id       id of the item to update
+     * @param incoming the new item
+     * @return the updated item object
+     */
+    @Operation(summary= "Update a single item")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public UCSBDiningCommonsMenuItem updateUCSBDiningCommonsMenuItem(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid UCSBDiningCommonsMenuItem incoming) {
+
+                UCSBDiningCommonsMenuItem ucsbDiningCommonsMenuItem = ucsbDiningCommonsMenuItemRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBDiningCommonsMenuItem.class, id));
+
+        ucsbDiningCommonsMenuItem.setDiningCommonsCode(incoming.getDiningCommonsCode());
+        ucsbDiningCommonsMenuItem.setName(incoming.getName());
+        ucsbDiningCommonsMenuItem.setStation(incoming.getStation());
+
+        ucsbDiningCommonsMenuItemRepository.save(ucsbDiningCommonsMenuItem);
+
+        return ucsbDiningCommonsMenuItem;
+    }
 }
